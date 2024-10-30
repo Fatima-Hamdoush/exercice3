913f1e014606808f3fb5e3c2a366f5c9210a2bb5
Fatima Hamdouch <hamdouchfatima168@gmail.com>
2024-10-30 14:45:35 +0200

    exercice3 is commited

---
+++ diamond.py
@@ -0,0 +1,19 @@
+h = int (input(" please enter he rows number:"))
+#       -*-
+#      -***-
+#     -*****-
+#     ******* for i ==4
+#      *****
+#       ***
+#        *
+for i in range(h):
+    print(" "*(h-i), "*"*(i*2+1))
+for i in range(h-2, -1, -1):
+    print(" "*(h-i), "*"*(i*2+1))
+
+       
+         
+
+    
+
+
